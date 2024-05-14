c7972ab32f53309d852c51a74146c0ba17a88045
Tiitmouse <lkovacic380@gmail.com>
2024-05-14 12:07:26 +0200

    initional commit

---
+++ fakeTiktok/App/fakeTiktokApp.swift
@@ -0,0 +1,17 @@
+//
+//  fakeTiktokApp.swift
+//  fakeTiktok
+//
+//  Created by Lorena Kovačić on 14.05.2024..
+//
+
+import SwiftUI
+
+@main
+struct fakeTiktokApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
